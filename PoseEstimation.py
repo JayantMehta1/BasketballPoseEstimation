4b6af54c2a52d05016dbc99ca66fca7a5c93f2e3
Azim Jiwani <azimjiwani4@gmail.com>
2023-11-22 15:47:06 -0500

    working mediapipe version

--- PoseEstimation.py
+++
@@ -1,7 +0,0 @@
-print("Pose Estimation")
-
-# https://web.stanford.edu/class/cs231a/prev_projects_2022/lCS231a_Project_Final_Report.pdf
-
-# https://docs.google.com/document/d/1DCO9DQzoMXnh2wSPDg1ADRn4dPvAWdFWVOq4AuSWesQ/edit
-
-# https://github.com/chonyy/AI-basketball-analysis
